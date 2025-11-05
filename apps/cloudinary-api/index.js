b7e8fc83d13bb8fe1d9968f63e921e61504b057e
Ieremciuc Ciprian Stefan <ieremciuc.ciprian@gmail.com>
2025-11-06 01:55:05 +0200

    Cloudinary API #11

--- apps/cloudinary-api/index.js
+++ apps/cloudinary-api/index.js
@@ -37,7 +37,7 @@ app.post("/upload", upload.single("file"), async (req, res) => {
 // });
 
 // Fetch file info by public_id
-app.get("/file/:path(*)", async (req, res) => {
+app.get("/file/*", async (req, res) => {
   try {
     const public_id = req.params.path;
     const info = await cloudinary.api.resource(public_id);
