0b91fac33c97a5d312b1194a953241b0e5280ab1
Ieremciuc Ciprian Stefan <ieremciuc.ciprian@gmail.com>
2025-11-06 02:38:03 +0200

    Debugging #2

--- apps/cloudinary-api/index.js
+++ apps/cloudinary-api/index.js
@@ -40,8 +40,8 @@ app.post("/upload", upload.single("file"), async (req, res) => {
 app.get("/file/*path", async (req, res) => {
   try {
     const public_id = req.params.path;
-    public_id.replaceAll(",", "/");
-    const info = await cloudinary.api.resource(encoded_id);
+    const info = await cloudinary.api.resource(public_id);
+    info.public_id = public_id;
     res.json(info);
   } catch (error) {
     console.error(error);
