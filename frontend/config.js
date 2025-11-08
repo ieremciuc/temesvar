e84386c573140c947b08e2a2491418fee40a43cb
Ieremciuc Ciprian Stefan <ieremciuc.ciprian@gmail.com>
2025-11-08 18:30:52 +0200

    Link moved to config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,3 @@
 export const SUPABASE_API_URL = "https://supabase-api-production-322d.up.railway.app";
 export const CLOUDINARY_API_URL = "https://cloudinary-api-production-f738.up.railway.app";
+export const CLOUNDINARY_URL = "https://res.cloudinary.com/dsmrwftcx/image/upload/"
